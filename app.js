69812d40b784d8e3094d74c5d251ba9e92ba87b8
Amit-172000 <kanhaiyayadav172000@gmail.com>
2023-03-13 09:11:59 +0530

    done

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var http = require('http');
   
 http.createServer(function (req, res) {  
     res.write('Hello World!\n'); //write a response to the client  
-    res.write('this is V2 of NodeApp');
+    res.write('this is V2 of NodeApp\n');
+    res.write('this is V3');
     
     res.end(); //end the response  
 }).listen(8080); //the server object listens on port 8080   
